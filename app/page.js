1657f0b5247302cb47a767e753144c4151f525e0
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-03-02 02:46:38 +0600

    added an rough api fetch code

--- app/page.js
+++ app/page.js
@@ -1,9 +1,37 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react"
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello</h1>
-    </main>
-  );
+	const [result, setResult]=useState(null)
+	// Replace YOUR_API_KEY with your actual API key if required (some APIs might require an API key).
+	const API_KEY = 1;
+	const API_URL = 'https://www.thecocktaildb.com/api/json/v1/' + API_KEY;
+
+	// Function to fetch data from the Cocktail DB API
+	async function fetchCocktailData(cocktailName) {
+		try {
+			const res = await fetch(`${API_URL}/search.php?s=${cocktailName}`);
+			
+			if (!res.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+
+			const data = await res.json();
+			
+			// Process the JSON data
+			console.log((data.drinks));
+			setResult(data.drinks[0].strDrink)
+			return data;
+		} catch (error) {
+			console.error('Error fetching data:', error.message);
+		}
+	}
+
+fetchCocktailData("gin and tonic")
+return (
+	<div className=""> {result}</div>
+);
 }
+
