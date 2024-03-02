5193548039c7b8ca97549aed14086f1df7d3fb30
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-03-02 22:42:23 +0600

    made a rough api call with router for search cocktail endpoint

--- app/page.js
+++ app/page.js
@@ -5,33 +5,52 @@ import { useState } from "react"
 
 export default function Home() {
 	const [result, setResult]=useState(null)
-	// Replace YOUR_API_KEY with your actual API key if required (some APIs might require an API key).
-	const API_KEY = 1;
-	const API_URL = 'https://www.thecocktaildb.com/api/json/v1/' + API_KEY;
-
-	// Function to fetch data from the Cocktail DB API
-	async function fetchCocktailData(cocktailName) {
+	const [cockTailToSearchText,setCockTailToSearchText] =useState("")
+	
+	// To fetch data from the cocktail DB API
+	async function fetchCocktailData(cockTailToSearch) {
+		
 		try {
-			const res = await fetch(`${API_URL}/search.php?s=${cocktailName}`);
+			// conneting with route.js and sending the parameters 
+			const res = await fetch(`/api/cocktail`, {
+				method:`Post`,
+				body: JSON.stringify({search:cockTailToSearch})
+			})
+			// console.log(res)
 			
 			if (!res.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
 			}
-
 			const data = await res.json();
 			
 			// Process the JSON data
-			console.log((data.drinks));
-			setResult(data.drinks[0].strDrink)
-			return data;
+			console.log(data);
+			
+			setResult(JSON.stringify(data))
+			// return data;
 		} catch (error) {
 			console.error('Error fetching data:', error.message);
 		}
-	}
 
-fetchCocktailData("gin and tonic")
-return (
-	<div className=""> {result}</div>
-);
+	}
+	return (
+		<div className="">
+			<div>
+				<input
+				className="text-black "
+				placeholder="enter cocktail name"
+				type="text"
+				onChange={(e) => setCockTailToSearchText(e.target.value)}
+				></input>
+				<button
+				className="border-2 rounded-full bg-sky-500 text-black"
+				onClick={() =>fetchCocktailData(cockTailToSearchText)}
+				>Search</button>
+			</div>
+			<div>
+				<div>{result}</div>
+			</div>
+		</div>
+	);
 }
 
