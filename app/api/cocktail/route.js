f35b549167e3f39ec76a796bb4c616279c4f6e12
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-03-05 09:44:50 +0600

    save point for i m gonna alter all of the code

--- app/api/cocktail/route.js
+++ app/api/cocktail/route.js
@@ -1,21 +1,26 @@
 const BASE_URL ='https://thecocktaildb.com/api/json/v1'
 const API_KEY= process.env.API_KEY
-const SEARCH_COCKTAIL="search.php?s="
+// const searchParameter="search.php?s="
 
 export async function POST(request){
-    const {search} = await request.json();
+    const {search, parameter} = await request.json();
+    // console.log(search, parameter)
     const encodedSearch=encodeURIComponent(search)
+    // console.log(`${BASE_URL}/${API_KEY}/${parameter}${encodedSearch}`)
     
-    const res = await fetch(`${BASE_URL}/${API_KEY}/${SEARCH_COCKTAIL}${encodedSearch}`,{
+    const res = await fetch(`${BASE_URL}/${API_KEY}/${parameter}${encodedSearch}`,{
         method: 'post',
-        body: JSON.stringify({ query: search}),
+        body: JSON.stringify({query: search}),
         headers:{
             "content-type":"application/json"
         }
     })
-    // console.log(`${BASE_URL}/${API_KEY}/${SEARCH_COCKTAIL}${encodedSearch}`)
-    
     const data= await res.json()
-    // console.log(data.drinks[0])
-    return Response.json({data: data?.drinks[0]})
+    if(parameter === "search.php?i="){
+        // console.log(data +"here")
+        return Response.json({data: data?.ingredients})
+    }else{
+        // console.log(data +"down here")
+        return Response.json({data: data?.drinks})
+    }
 }
