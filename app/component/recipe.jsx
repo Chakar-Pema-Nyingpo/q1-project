7502ea0dc887851e0e793fb9a80ec69029908be2
Pema Tsheten <hooligancrew140@gmail.com>
2024-03-06 16:23:57 +0600

    container

---
+++ app/component/recipe.jsx
@@ -0,0 +1,22 @@
+// export default function recipe({data}) {
+//     return (
+//         <div className="grid grid-cols-3 gap-4">
+//         {data.drinks.map((obj)=>{
+//             return (
+//             <div className="p-10 pb-3 border-2 bg-red-100 w-fit text-center ">
+//             <a>
+//                 <div>
+//                 <img src={obj.strDrinkThumb} className="w-64"></img>
+//                 <a href="#"><p className="mt-3 text-lg text-black font-serif">{obj.strDrink}</p></a>
+//                 </div>
+//             </a>
+//             </div>
+
+//             )
+//         })
+//         }
+//         </div>    
+//     )
+// }
+
+	
