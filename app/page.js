c6e275db23937527e123e792200d4a0538ed5667
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-03-07 01:36:39 +0600

    UI and Backend conneted

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
-// "use client"
-
+"use client"
+import { BsSearch } from "react-icons/bs";
+import { IoMdHome } from "react-icons/io";
+import { FaMartiniGlassCitrus } from "react-icons/fa6";
+import { FaFilePen } from "react-icons/fa6";
 import { useState } from "react"
+
 
 import ListComponent from "./component/ListCategory.jsx"
 
 const NAME = "name"
 const INGREDIENTS = "ingredients"
-const LIST = "list"
+const HOME = "list"
 
 
 export default function Home() {
 
-	const [appState, setAppState] = useState(LIST)
+	const [appState, setAppState] = useState(HOME)
 	const [result, setResult]=useState(null)
 	const [cockTailToSearchText,setCockTailToSearchText] = useState()
 	const [searchParameter, setSearchParamter] = useState("list.php?c=")
@@ -26,10 +30,10 @@ export default function Home() {
 			})
 			// console.log(res)
 			
-// 			if (!res.ok) {
-// 			throw new Error(`HTTP error! Status: ${response.status}`);
-// 			}
-// 			const data = await res.json();
+			if (!res.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			const data = await res.json();
 			
 			// Process the JSON data
 			setResult(data)
@@ -39,7 +43,7 @@ export default function Home() {
 		}
 		
 	}
-	if (appState === LIST){
+	if (appState === HOME){
 	 fetchCocktailData("list")
 	//  console.log(listData)
 	}
@@ -52,218 +56,133 @@ export default function Home() {
 
 
 	return (
-		<div className="">
-			<div>
+		<div className="bg-field bg-cover"> 
+		<nav className= "mb-2 bg-[#B66876] max-w-full h-20 flex text-nowrap pl-2 justify-between items-center">
+			<div className="text-grey-500 md:text-3xl text-xl font-bold text-black flex-none">
+				The Cocktail
+			</div>
 			
-				<button
-					className={"border-2 rounded-full text-white px-4 mx-2 "+(appState === NAME ? "bg-sky-900" : "bg-sky-400")}
-					onClick={() =>handleClick(NAME, "search.php?s=")}
-					>By Name</button>
+			{appState === HOME ? (
+				<div className="md:hidden">
 
-				<button
-					className={"border-2 rounded-full text-white px-4 mx-2 "+(appState === INGREDIENTS? "bg-sky-900":"bg-sky-400")}
-					onClick={() =>handleClick(INGREDIENTS, "filter.php?i=")}
-					>By Ingredients</button>
-
-				<button
-					className={"border-2 rounded-full text-white px-4 mx-2 "+(appState === LIST? "bg-sky-900":"bg-sky-400")}
-					onClick={() =>handleClick(LIST,"list.php?c=")}
-					>List Category</button>
-
-				{appState === LIST ? (
-					<></>
-				) : (null)}
-					
-				{appState === NAME ? (
-					
-					<div className="my-5">
-					<input
-						className="text-black "
-						placeholder="Enter Cocktail name"
-						type="text"
-						onChange={(e) => setCockTailToSearchText(e.target.value)}
-					/>
-					<button
-						className="border-2 rounded-full bg-sky-500 text-black px-4 mx-2"
-						onClick={() =>fetchCocktailData(cockTailToSearchText)}
-						>Search
-					</button>
-					</div>
-				) : (null)}
-
-				{appState === INGREDIENTS ? (
-					<div className="my-5">
-					<input
-						className="text-black "
-						placeholder="Enter Ingredients name"
-						type="text"
-						onChange={(e) => setCockTailToSearchText(e.target.value)}
-					/>
-					<button
-						className="border-2 rounded-full bg-sky-500 text-black px-4 mx-2"
-						onClick={() =>fetchCocktailData(cockTailToSearchText)}
-						>Search
-					</button>
-					</div>
-				) : (null)}
-			</div>
-			<div>
-				<div>
-					<ListComponent
-						list={result}
-					/>
 				</div>
+			) : (null)}
+				
+			{appState === NAME ? (
+				<div>
+					<div className="md:hidden">
+						<input
+							className="text-center text-black focus:outline-none text-xl bg-slate-300 mx-2 w-4/5 rounded-md  "
+							type="text"
+							placeholder="Enter Cocktail"
+							onChange={(e) => setCockTailToSearchText(e.target.value)}
+						/>
+						<button onClick={fetchCocktailData(cockTailToSearchText)}>
+							<BsSearch size={20} className="text-white pt-1" />
+						</button>
+					</div>
+					<div className="placeholder:center md:mx-auto " >
+						<div className="flex justify-between items-center max-w-[500px] w-full text-white ">
+							<form
+								onSubmit={fetchCocktailData(cockTailToSearchText)}
+								className="md:flex hidden my-4 justify-between items-center w-full p-3 border border-slate-300 rounded-md py-2 pl-9 pr-3 focus:outline-none focus:border-rose-400 focus:ring-rose-400 focus:ring-1">
+								<div>
+									<input
+										className="text-center bg-transparent border-none text-black focus:outline-none text-2xl "
+										type="text"
+										onChange={(e) => setCockTailToSearchText(e.target.value)}
+										placeholder="Enter Cocktail"
+									/>
+								</div>
+								<button onClick={fetchCocktailData(cockTailToSearchText)}>
+									<BsSearch size={20} className="text-black" />
+								</button>
+							</form>
+						</div>
+					</div>
 			</div>
-		</div>
-	);
+			) : (null)}
 
-import { useState } from "react";
-import { BsSearch } from "react-icons/bs";
-import { IoMdHome } from "react-icons/io";
-import { FaMartiniGlassCitrus } from "react-icons/fa6";
-import { FaFilePen } from "react-icons/fa6";
-import Image from "next/image";
+			{appState === INGREDIENTS ? (
+				<div>
+					<div className="md:hidden">
+						<input
+							className="text-center text-black focus:outline-none text-xl bg-slate-300 mx-2 w-4/5 rounded-md  "
+							type="text"
+							placeholder="Enter Cocktail Ingredient"
+							onChange={(e) => setCockTailToSearchText(e.target.value)}
+						/>
+						<button onClick={fetchCocktailData(cockTailToSearchText)}>
+							<BsSearch size={20} className="text-white pt-1" />
+						</button>
+					</div>
+					<div className="placeholder:center md:mx-auto " >
+						<div className="flex justify-between items-center max-w-[500px] w-full text-white ">
+							<form
+								onSubmit={fetchCocktailData(cockTailToSearchText)}
+								className="md:flex hidden my-4 justify-between items-center w-full p-3 border border-slate-300 rounded-md py-2 pl-9 pr-3 focus:outline-none focus:border-rose-400 focus:ring-rose-400 focus:ring-1">
+								<div>
+									<input
+										className="text-center bg-transparent border-none text-black focus:outline-none text-2xl "
+										type="text"
+										onChange={(e) => setCockTailToSearchText(e.target.value)}
+										placeholder="Enter Cocktail Ingredient"
+									/>
+								</div>
+								<button onClick={fetchCocktailData(cockTailToSearchText)}>
+									<BsSearch size={20} className="text-black" />
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			) : (null)}	
+			</nav>
 
-export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [cocktail, setCocktail] = useState(null);
+			<div className="flex flex-col-reverse md:flex-row">
+				<nav className= "md:pl-2 items-center rounded-t-[30px] bg-[#B66876] h-20 md:w-16 md:gap-4 md:h-1/2 md:py-4 flex sticky bottom-0 md:flex-col md:justify-around mt-44 md:rounded-full md:ml-2">
 
-  const fetchCocktail = (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const url = `http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
-    fetch(url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setCocktail(data);
-        console.log(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching cocktail data:", error);
-        setLoading(false);
-      });
-  };
+					<button 
+					title="Home" 
+					className={"mx-auto my-2 h-14 w-14 overflow-hidden border border-[#B66876] text-rose-[#B66876] shadow-2xl transition-all duration-200 relative before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-rose-400 before:duration-300 before:ease-out hover:text-white hover:before:h-9 hover:before:w-9 hover:before:opacity-25"+(appState === NAME ? "bg-sky-900" : "")}
+					onClick={() =>handleClick(HOME, "list.php?c=")}
+					>
+					
+						<IoMdHome size={36} className="text-black ml-1"/>
+					</button>
 
-  return (
+					<button 
+					title="Cocktails" 
+					className={"mx-auto my-2 h-14 w-14 overflow-hidden border border-[#B66876] text-rose-[#B66876] shadow-2xl transition-all duration-200 relative before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-rose-400 before:duration-300 before:ease-out hover:text-white hover:before:h-9 hover:before:w-9 hover:before:opacity-25"+(appState === NAME ? "bg-sky-900" : "")}
+					onClick={() =>handleClick(NAME, "search.php?s=")}
+					>
+						<FaMartiniGlassCitrus size={36} className="text-black ml-1" />
+					</button>
 
-    <>
-    
-    <div className="bg-field bg-cover"> 
-     <nav className= "mb-2 bg-[#B66876] max-w-full h-20 flex text-nowrap pl-2 justify-between items-center">
-      <div className="text-grey-500 md:text-3xl text-xl font-bold text-black flex-none">The Cocktail</div>
-      <div className="md:hidden">
-        <input
-          className="text-center text-black focus:outline-none text-xl bg-slate-300 mx-2 w-4/5 rounded-md  "
-          type="text"
-          placeholder="Enter Cocktail"
-          onChange={(e) => setCity(e.target.value)}
-        ></input>
-      <button onClick={fetchCocktail}>
-        <BsSearch size={20} className="text-white pt-1" />
-      </button>
-      </div>
-
-      <div className="placeholder:center md:mx-auto " >
-
-      <div className="flex justify-between items-center max-w-[500px] w-full text-white ">
-        <form
-          onSubmit={fetchCocktail}
-          className="md:flex hidden my-4 justify-between items-center w-full p-3 border border-slate-300 rounded-md py-2 pl-9 pr-3 focus:outline-none focus:border-rose-400 focus:ring-rose-400 focus:ring-1"
-        >
-          <div>
-            <input
-              className="text-center bg-transparent border-none text-black focus:outline-none text-2xl "
-              type="text"
-              placeholder="Enter Cocktail"
-          
-            ></input>
-          </div>
-          <button onClick={fetchCocktail}>
-            <BsSearch size={20} className="text-black" />
-          </button>
-
-        </form>
-      </div>
-
-      {/* <input 
-      placeholder="Enter Cocktail "
-      className= " my-4 ml-64 text-center text-black italic placeholder:text-slate-400 block bg-slate-300 w-96 border border-slate-300 rounded-md py-2 pl-9 pr-3 focus:outline-none focus:border-rose-400 focus:ring-rose-400 focus:ring-1 sm:text-sm "
-      >
-      
-      </input> */}
-      
-
-      
-      </div>
-    </nav>
-
-
-    <div className="flex flex-col-reverse md:flex-row">
-      <nav className= "md:pl-2 items-center rounded-t-[30px] bg-[#B66876] h-20 md:w-16 md:gap-4 md:h-1/2 md:py-4 flex sticky bottom-0 md:flex-col md:justify-around mt-44 md:rounded-full md:ml-2">
-
-      <button title="Home" className="mx-auto my-2 h-14 w-14 overflow-hidden border border-[#B66876] text-rose-[#B66876] shadow-2xl transition-all duration-200 relative before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-rose-400 before:duration-300 before:ease-out hover:text-white hover:before:h-9 hover:before:w-9 hover:before:opacity-25">
-        <IoMdHome size={36} className="text-black ml-1" />
-        </button>
-
-        <button title="Cocktails" className="mx-auto my-2 h-14 w-14 overflow-hidden border border-[#B66876] text-rose-[#B66876] shadow-2xl transition-all duration-200 relative before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-rose-400 before:duration-300 before:ease-out hover:text-white hover:before:h-9 hover:before:w-9 hover:before:opacity-25">
-        <FaMartiniGlassCitrus size={36} className="text-black ml-1" />
-        </button>
-
-        <button title="Ingredients" className="md:pl-1 mx-auto my-2 h-14 w-14 overflow-hidden border border-[#B66876] text-rose-[#B66876] shadow-2xl transition-all duration-200 relative before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-rose-400 before:duration-300 before:ease-out hover:text-white hover:before:h-9 hover:before:w-9 hover:before:opacity-25">
-        <FaFilePen size={36} className="text-black ml-1" />
-        </button>
-      </nav>
-
-      <div className= "text-center w-full h-full md:pl-10 md:pt-8 grid grid-cols-1 gap-3 md:grid-cols-3 md:pr-5 ">
-        <div className=" rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
-            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-auto w-auto">
-            </img>
-            <p className="text-black mt-2"> margarita </p>
-        </div>
-        <div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
-            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34">
-            </img>
-            <p className="text-black mt-2"> margarita </p>
-        </div>
-        <div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
-            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34">
-            </img>
-            <p className="text-black mt-2"> margarita </p>
-        </div>
-        <div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
-            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34">
-            </img>
-            <p className="text-black mt-2"> margarita </p>
-        </div>
-        <div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
-            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34">
-            </img>
-            <p className="text-black mt-2"> margarita </p>
-        </div>
-        <div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
-            <img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34">
-            </img>
-            <p className="text-black mt-2"> margarita </p>
-        </div>
-       
-      </div>
-    </div>
-</div>
-    </>
-  );
+					<button 
+					title="Ingredients" 
+					className={"md:pl-1 mx-auto my-2 h-14 w-14 overflow-hidden border border-[#B66876] text-rose-[#B66876] shadow-2xl transition-all duration-200 relative before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-rose-400 before:duration-300 before:ease-out hover:text-white hover:before:h-9 hover:before:w-9 hover:before:opacity-25"+(appState === NAME ? "bg-sky-900" : "")}
+					onClick={() =>handleClick(NAME, "filter.php?i=")}
+					>
+						<FaFilePen size={36} className="text-black ml-1" />
+					</button>
+				</nav>
+	{/* TO BE GENERATED FROM A COMPONENT */}
+				<div className= "text-center w-full h-full md:pl-10 md:pt-8 grid grid-cols-1 gap-3 md:grid-cols-3 md:pr-5 ">
+					<div className=" rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
+						<img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-auto w-auto"/>
+						<p className="text-black mt-2"> margarita </p>
+					</div>
+					<div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
+						<img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34"/>
+						<p className="text-black mt-2"> margarita </p>
+					</div>
+					<div className="rounded-xl h-fit w-full bg-[#FFE7E9] p-8 pt-6 mb-2">
+						<img src="https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg" className="h-34 w-34"/>
+						<p className="text-black mt-2"> margarita </p>
+					</div>
+				</div>
+			</div> 
+		</div>// body end
+	)
 }
-
-
-
- 
-
-
-
-
-
