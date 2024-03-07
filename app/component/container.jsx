579f6fd318e2bbd4da27596bd87f422b9d76e6a5
Chakar-Pema-Nyingpo <114732678+Chakar-Pema-Nyingpo@users.noreply.github.com>
2024-03-07 09:25:22 +0600

    New Update

--- app/component/container.jsx
+++ app/component/container.jsx
@@ -5,12 +5,10 @@ export default function DisplayCocktail({data}) {
         {data.drinks.map((obj)=>{
             return (
             <div className="p-10 pb-3 border-2 bg-red-100 w-fit text-center">
-            <a>
                 <div>
                 <img src={obj.strDrinkThumb} className="w-64"></img>
                 <a href="#"><p className="mt-3 text-lg text-black font-serif">{obj.strDrink}</p></a>
                 </div>
-            </a>
             </div>
 
             )
