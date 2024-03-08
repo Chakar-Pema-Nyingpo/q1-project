b3c30dffa321500fe68d09efb7cb251b9a4a04a0
Sherab Namgay <144956521+SherabNamgay@users.noreply.github.com>
2024-03-08 14:02:13 +0600

    bug fix

--- app/component/DrinkCategory.jsx
+++ app/component/DrinkCategory.jsx
@@ -15,7 +15,7 @@ export default function DrinkCate({ list, setAppState, fetchCocktailData}) {
           <div className="text-center w-full h-full md:pt-8 grid grid-cols-1 gap-3 md:grid-cols-3 md:pr-5">
             {drinkDetails.map((obj) => (
               <div key={obj.idDrink} className="">
-                <div className="min-h-7 rounded-xl h-fit w-full md:gap-4 bg-[#FFE7E9] p-8 pt-6 mb-2 md:max-h-96 ">
+                <div className="min-h-7 rounded-xl h-fit w-full md:gap-4 bg-[#FFE7E9] p-8 pt-6 mb-2  ">
                 <button
                     className=""
                     onClick={()=>
@@ -23,7 +23,7 @@ export default function DrinkCate({ list, setAppState, fetchCocktailData}) {
                         setAppState(NAME)
                     }}>
                     <div className="md:min-h-96">
-                        <img src={obj.src} className="" alt={obj.name} />
+                        <img className=""src={obj.src} alt={obj.name} />
                         <p className="text-3xl font-bold text-black mt-2 trunacate">{obj.name}</p>
                     </div>   
                     </button>
